8b057ee5c8b5d3f0b049c3c7b6cbbfa15c24bbd1
Unknown <fragkasnikolaos@gmail.com>
2018-02-06 15:35:00 +0000

    Created a scene to work into and changed the player movement a bit

--- Build/PC/Player.cpp
+++ Build/PC/Player.cpp
@@ -3,13 +3,15 @@
 
 Player::Player() : GameObject("Player")
 {
+	life = maxLife;
+
 	playerGameObject = CommonUtils::BuildSphereObject("Player1",
 		Vector3(0.0f, 1.0f, 0.0f),
 		1.0f,									//Radius
 		true,									//Has Physics Object
 		1.0f,
 		true,									//Has Collision Shape
-		true,									//Dragable by the user
+		false,									//Dragable by the user
 		Vector4(0.5, 0.5, 0.5, 1.0));	//Color
 	canJump = true;
 }
@@ -20,6 +22,10 @@ Player::~Player()
 
 Player::Player(Vector3 pos, Colour c, float s) : GameObject("Player")
 {
+	colour = c;
+	size = s;
+	life = maxLife;
+
 	Vector4 Colour;
 	canJump = true;
 
@@ -59,11 +65,11 @@ Player::Player(Vector3 pos, Colour c, float s) : GameObject("Player")
 
 	playerGameObject = CommonUtils::BuildSphereObject("Player" + to_string(c),
 		Vector3(0.0f, 1.0f*s, 0.0f) + pos,
-		1.0f * s,								//Radius
+		1.0f * size,							//Radius
 		true,									//Has Physics Object
 		1.0f,
 		true,									//Has Collision Shape
-		true,									//Dragable by the user
+		false,									//Dragable by the user
 		Colour);								//Colour
 }
 
@@ -78,30 +84,36 @@ void Player::Input(float dt) {
 
 	playerGameObject->Physics()->SetOnCollisionCallback(SetCanJump);
 
+
 	float yaw = GraphicsPipeline::Instance()->GetCamera()->GetYaw();
 	float pitch = GraphicsPipeline::Instance()->GetCamera()->GetPitch();
 
+
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_W)) { 		//Front
+		if (force.z > 0)force.z /= 2;
+	//	force.y = 0;
+		force = Matrix3::Rotation(0, Vector3(10, 0, 0)) * Matrix3::Rotation(yaw, Vector3(0, 0, 0)) * Vector3(0, 0, -3) * speed;
+	}
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_S)) {		//Back
+		if (force.z < 0)force.z /= 2;
+	//	force.y = 0;
+		force = Matrix3::Rotation(0, Vector3(-10, 0, 0)) * Matrix3::Rotation(yaw, Vector3(0, 0, 0)) * Vector3(0, 0, 3) * speed;
+	}
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_A)) {		//Left
+		if (force.x > 0)force.x /= 2;
+	//	force.y = 0;
+		force = Matrix3::Rotation(0, Vector3(0, 0, -10)) * Matrix3::Rotation(yaw, Vector3(0, 0, 3)) * Vector3(-3, 0, 0) * speed;
+	}
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_D)) {		//Right
+		if (force.x < 0)force.x /= 2;
+	//	force.y = 0;
+		force = Matrix3::Rotation(0, Vector3(0, 0, 10)) * Matrix3::Rotation(yaw, Vector3(0, 0, 3)) * Vector3(3, 0, 0) * speed;
+	}
 	if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_SPACE) && canJump) {		//Jump
 		playerGameObject->Physics()->SetLinearVelocity(Vector3(force.x / 2, jumpImpulse, force.z / 2));
 		canJump = false;
 	}
 
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_W)) { 		//Front
-		if (force.z > 0)force.z /= 2;
-		force += Matrix3::Rotation(0, Vector3(10, 0, 0)) * Matrix3::Rotation(yaw, Vector3(0, 0, 0)) * Vector3(0, 0, -3) * speed * dt;
-	}
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_S)) {		//Back
-		if (force.z < 0)force.z /= 2;
-		force += Matrix3::Rotation(0, Vector3(-10, 0, 0)) * Matrix3::Rotation(yaw, Vector3(0, 0, 0)) * Vector3(0, 0, 3) * speed * dt;
-	}
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_A)) {		//Left
-		if (force.x > 0)force.x /= 2;
-		force += Matrix3::Rotation(0, Vector3(0, 0, -10)) * Matrix3::Rotation(yaw, Vector3(0, 0, 3)) * Vector3(-3, 0, 0) * speed * dt;
-	}
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_D)) {		//Right
-		if (force.x < 0)force.x /= 2;
-		force += Matrix3::Rotation(0, Vector3(0, 0, 10)) * Matrix3::Rotation(yaw, Vector3(0, 0, 3)) * Vector3(3, 0, 0) * speed * dt;
-	}
 
 
 	if (force.x > maxForce)force.x = maxForce;
@@ -109,4 +121,23 @@ void Player::Input(float dt) {
 	if (force.z > maxForce)force.z = maxForce;
 	if (force.z < -maxForce)force.z = -maxForce;
 	playerGameObject->Physics()->SetForce(force);
+	force = Vector3(0, 0, 0);
+}
+
+// Updates everything on player
+void Player::OnPlayerUpdate(float dt) {
+
+	Input(dt);
+
+
+	if (life > minLife) {
+		life -= 0.01;
+	}
+	
+	curSize = size * (life / 100);
+
+	cout << curSize;
+
+	playerGameObject->Render()->GetChild()->SetBoundingRadius(curSize);
+	playerGameObject->RegisterPhysicsToRenderTransformCallback();
 }
