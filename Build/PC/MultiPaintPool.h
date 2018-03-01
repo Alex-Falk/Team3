5f6d0b079131403fc3e59a70989faba9a7655316
Unknown <fragkasnikolaos@gmail.com>
2018-03-01 11:21:24 +0000

    start
    start

---
+++ Build/PC/MultiPaintPool.h
@@ -0,0 +1,24 @@
+//Nikos Fragkas
+//Date 01/03
+//Paint Pool that will have your colour as soon as you capture the objects arround
+
+#pragma once
+#include "PaintPool.h"
+
+class MultiPaintPool : public PaintPool
+{
+public:
+	MultiPaintPool();
+	~MultiPaintPool();
+
+private:
+
+};
+
+MultiPaintPool::MultiPaintPool()
+{
+}
+
+MultiPaintPool::~MultiPaintPool()
+{
+}
