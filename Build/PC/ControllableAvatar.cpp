46dc2b7501cb46e46e602a9c6b945c57b7ba2ca3
Nick <nickbedford@sky.com>
2018-03-13 17:15:13 +0000

    Removed ability to change weapon with keys

--- Build/PC/ControllableAvatar.cpp
+++ Build/PC/ControllableAvatar.cpp
@@ -80,11 +80,9 @@ void ControllableAvatar::ProcessAvatarInput(float dt)
 
 	
 	MovementState(move, yaw, dt);
-
-
-
-
-	if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_1)) {
+	
+	//TODO: DELETE THIS ONCE ALL DEBUGGING STOPS
+	/*if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_1)) {
 		NCLDebug::Log("Pistol Activated");
 		weapon = PAINT_PISTOL;
 	}
@@ -99,7 +97,7 @@ void ControllableAvatar::ProcessAvatarInput(float dt)
 	if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_4)) {
 		NCLDebug::Log("Auto Activated");
 		weapon = AUTO_PAINT_LAUNCHER;
-	}
+	}*/
 
 	if (Input::Instance()->GetInput(SHOOT))
 	{
